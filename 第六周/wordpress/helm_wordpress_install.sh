c8f668f8b46347a182d4b92755eace1cda1233b8
code-horse-mi <945623397@qq.com>
2024-02-07 12:26:41 +0800

    第六周作业

--- 第六周/wordpress/helm_wordpress_install.sh
+++ 第六周/wordpress/helm_wordpress_install.sh
@@ -7,3 +7,7 @@ root@master:~# helm install wordpress \
                oci://registry-1.docker.io/bitnamicharts/wordpress \
                -f wordpress-values.yaml \
                -n blog --create-namespace
+root@master:~# helm list -n blog
+NAME            NAMESPACE       REVISION        UPDATED                                 STATUS          CHART                   APP VERSION
+mysql           blog            1               2024-02-07 03:09:50.609364819 +0000 UTC deployed        mysql-9.19.1            8.0.36
+wordpress       blog            1               2024-02-07 04:08:46.524188345 +0000 UTC deployed        wordpress-19.2.3        6.4.3
