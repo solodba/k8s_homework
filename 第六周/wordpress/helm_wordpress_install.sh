1c37cf4749863654241b41756f4b3a4d5bd268b8
code-horse-mi <945623397@qq.com>
2024-02-07 12:20:24 +0800

    第六周作业

--- 第六周/wordpress/helm_wordpress_install.sh
+++ 第六周/wordpress/helm_wordpress_install.sh
@@ -3,3 +3,7 @@ root@master:~# apt update
 root@master:~# apt install -y nfs-common
 
 # helm部署wordpress服务
+root@master:~# helm install wordpress \
+               oci://registry-1.docker.io/bitnamicharts/wordpress \
+               -f wordpress-values.yaml \
+               -n blog --create-namespace
